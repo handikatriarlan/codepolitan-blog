76670f04b6b86f5fe86acea3920b931bfe7016f1
Arlan Tri Handika <handikaarlan@gmail.com>
2024-06-02 15:16:47 +0700

    add controller

---
+++ app/Http/Controllers/HelloController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HelloController extends Controller
+{
+    function index()
+    {
+        echo "Hello";
+    }
+
+    function world_message()
+    {
+        echo "World";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,19 @@
 <?php
 
+use App\Http\Controllers\HelloController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello', function () {
-    return view('hello');
-});
+// Route::get('hello', function () {
+//     return view('hello');
+// });
+
+// //Cara Pertama
+// Route::get('hello', 'App\Http\Controllers\HelloController@index');
+
+//Cara Kedua
+Route::get('hello', [HelloController::class, 'index']);
+Route::get('world', [HelloController::class, 'world_message']);
