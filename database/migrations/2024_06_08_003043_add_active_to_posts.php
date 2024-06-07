510b5750951e76da09ace40c1c88fcb4420f555d
Arlan Tri Handika <handikaarlan@gmail.com>
2024-06-08 00:40:46 +0700

    add default value

--- database/migrations/2024_06_08_003043_add_active_to_posts.php
+++ database/migrations/2024_06_08_003043_add_active_to_posts.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->boolean('active')->after('content');
+            $table->boolean('active')->after('content')->default(true);
         });
     }
 
