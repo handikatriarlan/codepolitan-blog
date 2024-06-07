680bb38dc878836fe658da800a974ce635049454
Arlan Tri Handika <handikaarlan@gmail.com>
2024-06-07 14:45:34 +0700

    fix code structure

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,9 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/blog.css') }}">
     <link href="{{ asset('bootstrap-5/css/bootstrap.min.css') }}" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ asset('css/blog.css') }}">
     <script src="{{ asset('bootstrap-5/js/bootstrap.bundle.min.js') }}"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
     </script>
@@ -22,7 +22,8 @@
 
             <p>{{ $post[2] }}</p>
         </article>
-        <a href="{{ url("posts") }}">< Kembali</a>
+        <a href="{{ url('posts') }}">
+            < Kembali</a>
     </div>
 </body>
 
