5e6b34639eb9b894a48b563ad22ab7cf304fc5bb
Arlan Tri Handika <handikaarlan@gmail.com>
2024-06-07 14:44:23 +0700

    add styling

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,21 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Blog</title>
+    <link rel="stylesheet" href="{{ asset('css/blog.css') }}">
     <link href="{{ asset('bootstrap-5/css/bootstrap.min.css') }}" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="{{ asset('bootstrap-5/js/bootstrap.bundle.min.js') }}"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
     </script>
-    <style>
-        .blog {
-            padding: 5px;
-            border-bottom: 1px solid lightgray;
-        }
-
-        small {
-            color: grey;
-        }
-    </style>
 </head>
 
 <body>
