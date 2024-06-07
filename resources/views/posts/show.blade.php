9bf518ea4391270e5718fa0a1999af9bc0e08f8a
Arlan Tri Handika <handikaarlan@gmail.com>
2024-06-08 00:22:02 +0700

    edit title

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -11,7 +11,7 @@
     <script src="{{ asset('bootstrap-5/js/bootstrap.bundle.min.js') }}"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
     </script>
-    <title>Blog | Judul: {{ $post->title }}</title>
+    <title>Blog | {{ $post->title }}</title>
 </head>
 
 <body>
