64722020d3080e6e49ceddcf2c4c9657d5ad9da2
Arlan Tri Handika <handikaarlan@gmail.com>
2024-06-16 01:06:00 +0700

    separating header & footer layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,29 +16,9 @@
 
 <body>
     <div class="container">
-        <header
-            class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-
-            <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-                <li><a href="{{ url('posts') }}" class="nav-link px-2 link-secondary">Beranda</a></li>
-                <li><a href="{{ url('posts/create') }}" class="nav-link px-2">Tambah Postingan</a></li>
-            </ul>
-
-            <div class="col-md-3 text-end">
-                <button type="button" class="btn btn-outline-primary me-2">Login</button>
-            </div>
-        </header>
-
+        @include('layouts.app.header')
         @yield('content')
-
-        <footer class="py-3 my-4">
-            <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Beranda</a></li>
-                <li class="nav-item"><a href="{{ url('posts/create') }}"
-                        class="nav-link px-2 text-body-secondary">Tambah Postingan</a></li>
-            </ul>
-            <p class="text-center text-body-secondary">©2024 Codepolitan, Inc.</p>
-        </footer>
+        @include('layouts.app.footer')
     </div>
 </body>
 
