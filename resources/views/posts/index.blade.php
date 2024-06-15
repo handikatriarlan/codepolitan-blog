a486d1f4d999eeb17f9981576f6337bcb364b518
Arlan Tri Handika <handikaarlan@gmail.com>
2024-06-16 00:41:11 +0700

    add footer and header

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -15,31 +15,46 @@
 </head>
 
 <body>
-    <div class="container mt-5">
-        <h1 class="mb-4 d-flex justify-content-between align-items-center">
-            Blog Codepolitan
-            <a class="btn btn-success" href="{{ url('posts/create') }}">Tambah Postingan</a>
-        </h1>
-        @foreach ($posts as $post)
-            <div class="card mb-3 shadow-sm">
-                <div class="card-body">
-                    <p class="card-text"><small class="text-muted">Created at
-                            {{ date('d M Y H:i', strtotime($post->created_at)) }}</small></p>
-                    <h5 class="card-title">{{ $post->title }}</h5>
-                    <p class="card-text">{{ $post->content }}</p>
-                    <p class="card-text"><small class="text-muted">Last updated at
-                            {{ date('d M Y H:i', strtotime($post->updated_at)) }}</small></p>
-                    <a href="{{ url("posts/$post->id") }}" class="btn btn-primary">Selengkapnya</a>
-                </div>
-            </div>
-        @endforeach
-    </div>
+    <div class="container">
+        <header
+            class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
 
-    <footer class="footer text-center text-lg-start">
-        <div class="container p-4">
-            <p class="text-center mb-0">© 2024 Blog Codepolitan. All rights reserved.</p>
+            <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+                <li><a href="#" class="nav-link px-2 link-secondary">Beranda</a></li>
+                <li><a href="{{ url('posts/create') }}" class="nav-link px-2">Tambah Postingan</a></li>
+            </ul>
+
+            <div class="col-md-3 text-end">
+                <button type="button" class="btn btn-outline-primary me-2">Login</button>
+            </div>
+        </header>
+        <div class="container mt-5">
+            <h1 class="mb-4 d-flex justify-content-between align-items-center">
+                Blog Codepolitan
+            </h1>
+            @foreach ($posts as $post)
+                <div class="card mb-3 shadow-sm">
+                    <div class="card-body">
+                        <p class="card-text"><small class="text-muted">Created at
+                                {{ date('d M Y H:i', strtotime($post->created_at)) }}</small></p>
+                        <h5 class="card-title">{{ $post->title }}</h5>
+                        <p class="card-text">{{ $post->content }}</p>
+                        <p class="card-text"><small class="text-muted">Last updated at
+                                {{ date('d M Y H:i', strtotime($post->updated_at)) }}</small></p>
+                        <a href="{{ url("posts/$post->id") }}" class="btn btn-primary">Selengkapnya</a>
+                    </div>
+                </div>
+            @endforeach
         </div>
-    </footer>
+
+        <footer class="py-3 my-4">
+            <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Beranda</a></li>
+                <li class="nav-item"><a href="{{ url('posts/create') }}" class="nav-link px-2 text-body-secondary">Tambah Postingan</a></li>
+            </ul>
+            <p class="text-center text-body-secondary">©2024 Codepolitan, Inc.</p>
+        </footer>
+    </div>
 </body>
 
 </html>
